e5ca57d87731c93f5bae1872027ccb5dd58a78f1
jailtonmonteiro <jailtonmonteiro147@gmail.com>
2025-04-19 21:10:28 -0300

    Criação de classe para manipulação, e upload de arquivos para disponibilização no github.

--- scripts/dados_repos.py
+++ scripts/dados_repos.py
@@ -1,7 +1,17 @@
 from processamento_dados import DadosRepositorios
+from manipula_repos import ManipulaRepositorios
 
 tokenGit = 'token_git' # Cole aqui o seu token da API do GitHub
-contaGit = 'amzn' # Digite aqui a conta da empresa buscada
+contaGit = 'openai' # Digite aqui a conta da empresa buscada
+usernameGit = 'jailtonmonteiro' # Digite aqui o seu nome de usuário do GitHub
 
+# Manipulando dados
 Reps = DadosRepositorios(contaGit, tokenGit).criarDataFrame()
 print('Finalizado a extração e salvamentos dos dados')
+
+# Manipulando repositórios
+novoRepo = ManipulaRepositorios(usernameGit, tokenGit)
+nome_repo = 'linguagens-repositorios-empresas'
+
+novoRepo.cria_repo(nome_repo)
+novoRepo.add_arquivo(nome_repo, f'{contaGit}.csv', f'data_processed/{contaGit}.csv')
